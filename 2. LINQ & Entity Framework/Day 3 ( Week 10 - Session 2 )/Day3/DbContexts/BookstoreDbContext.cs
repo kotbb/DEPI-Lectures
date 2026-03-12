05535c59738cb1d04090f9d8c981bbc51277439f
Mohamed Kotb <kotb200484@gmail.com>
2026-03-12 04:02:16 +0200

    Add Assignment Day 3 LINQ & E.F

---
+++ 2. LINQ & Entity Framework/Day 3 ( Week 10 - Session 2 )/Day3/DbContexts/BookstoreDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Day3.Models;
+
+namespace Day3.DbContexts
+{
+    internal class BookstoreDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.; Database=BookstoreDB; Trusted_Connection=True; TrustServerCertificate=True;");
+        }
+        public DbSet <Book> Books { get; set; }
+        public DbSet <Author> Authors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>(D =>
+            {
+                D.Property(b => b.Title)
+                 .IsRequired()
+                 .HasMaxLength(150);
+
+                D.Property(b => b.Price)
+                 .HasColumnType("decimal(8,2)");
+
+                D.Property(b => b.PublishedDate)
+                 .IsRequired(false);
+            });
+        }
+    }
+}
