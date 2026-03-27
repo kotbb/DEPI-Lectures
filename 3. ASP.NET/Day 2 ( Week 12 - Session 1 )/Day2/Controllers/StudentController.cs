c1520d146d2b172ffbdb3b49112ac197351cb84c
Mohamed Kotb <kotb200484@gmail.com>
2026-03-27 07:02:29 +0200

    Add Assignment Day 2 ASP.NET

---
+++ 3. ASP.NET/Day 2 ( Week 12 - Session 1 )/Day2/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using Day2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day2.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult ShowAll()
+        {
+            StudentBL studentBL = new StudentBL();
+            List<Student> students = studentBL.GetAll();
+            return View("ShowAll", students);
+        }
+
+        public IActionResult ShowDetails(int id)
+        {
+            StudentBL studentBL = new StudentBL();
+            Student student = studentBL.GetById(id);
+            return View("ShowDetails", student);
+        }
+    }
+}
---
+++ 3. ASP.NET/Day 2 ( Week 12 - Session 1 )/Day2/Data/Contexts/SchoolDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Day2.Models;
+namespace Day2.Data.Contexts
+{
+    public class SchoolDbContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<StuCrsRes> StuCrsRes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=School;Integrated Security=True;Encrypt=False;Trust Server Certificate=True");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StuCrsRes>()
+            .HasKey(scr => new { scr.StudentId, scr.CourseId });
+        }
+    }
+}
